47a561a71b7677d4c8f07505ad6cd7a75b04c47c
Priyanshu <agarwal3014@gmail.com>
2023-11-12 01:49:17 +0530

    added routes

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from prediction import predict_disease,similar
+
+class Input(BaseModel):
+    disease:list
 
 app=FastAPI()
 
 @app.get('/')
 def root():
     return {"message":"Root"}
+
+@app.post('/disease')
+def create_item(input:Input):
+    try:
+        result=predict_disease(input.disease)
+        return result
+    except Exception as e:
+        return ({'error':str(e)}),500
 
--- prediction.py
+++ prediction.py
@@ -1,29 +1,36 @@
 import pandas as pd
 import pickle
+# import sklearn
 
 def predict_disease(config):
-    df=pd.read_csv('./Final_Train_data.csv')
+    df=pd.read_csv('../Final_Train_data.csv')
     df.drop(axis=1,columns=['Unnamed: 0','fluid_overload'],inplace=True)
 
-    pkl_filename1="Project_model.pkl"
-    pkl_filename2="encoder.pkl"
+    pkl_filename1="../Project_model.pkl"
+    pkl_filename2="../encoder.pkl"
 
     with open(pkl_filename1,'rb') as f_in:
         model=pickle.load(f_in)
 
     with open(pkl_filename2,'rb') as e_in:
-        encoder=pickle.load(e_in)   
-
-    inp = pd.DataFrame({i:0 for i in df.drop('prognosis',axis=1).columns},index=[0])
-    for symptoms in config:
-        inp[symptoms]=1     
+        encoder=pickle.load(e_in) 
     
-    ans=encoder.inverse_transform(model.predict(inp))
-    return ans
+    for symptoms in config:
+        df.update({symptoms:1})    
+
+    inp = {i:0 for i in df.drop('prognosis',axis=1).columns}
+
+    # for symptoms in config:
+    #     inp.update({symptoms:1})
+
+    inp=pd.DataFrame(inp,index=[0])
+    # ans=encoder.inverse_transform(model.predict(inp))
+    # return ans
+    return 'ans received'
 
 def similar(dis):
-    data=pd.read_csv('./data_for_sim_dos.csv')
-    similarity=pd.read_csv('./similarity.csv')
+    data=pd.read_csv('../data_for_sim_dos.csv')
+    similarity=pd.read_csv('../similarity.csv')
 
     idx = data.index.to_list().index(dis)
     distances = similarity[idx]
